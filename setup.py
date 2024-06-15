91876e93678dd0d552c37b4e562ea814698446a7
arch-elite <168096365+arch-elite@users.noreply.github.com>
2024-06-15 13:25:41 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'University chatbot',
+    version= '0.0.0',
+    author= 'Earvin owiino',
+    author_email= 'earvinmobowino@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+) 
